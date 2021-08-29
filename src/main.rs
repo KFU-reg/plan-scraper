24f1dc810e7ce42343076e66c5f164650291bbdd
kfulain <89740818+stadial@users.noreply.github.com>
2021-08-29 18:54:05 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://httpbin.org/ip").await?.text().await?;
+    println!("{:#?}", resp);
+    Ok(())
+}
