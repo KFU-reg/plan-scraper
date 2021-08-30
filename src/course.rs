43788a1ab80085ba902e5d8d1fe23b4e183a8d91
kfulain <89740818+stadial@users.noreply.github.com>
2021-08-30 16:12:02 +0300

    course yada yad

---
+++ src/course.rs
@@ -0,0 +1,88 @@
+use select::document::Document;
+use select::node::Node;
+// use select::predicate::{Attr, Class, Name, Predicate};
+use select::predicate::Name;
+
+#[derive(Debug, Clone)]
+pub struct Course {
+    pub code: usize,
+    pub name: String,
+    pub credits: usize,
+    pub pre_requisites: Vec<usize>,
+    pub co_requisites: Vec<usize>,
+    pub semster: Semster,
+}
+
+#[derive(Debug, Clone)]
+pub struct Semster {
+    pub order: usize,
+    pub name: String,
+}
+
+impl<'a> From<Node<'a>> for Course {
+    /// Takes a `<tr>` (table row) Node
+    fn from(courses: Node<'a>) -> Self {
+        let course = courses.find(Name("td")).collect::<Vec<Node>>();
+        let code = course[0].text().parse::<usize>().unwrap();
+        let name = course[1].text();
+        let credits = course[2].text().parse::<usize>().unwrap();
+
+        // gets the PreRequists Node
+        let mut pre_requisites: Vec<usize> = vec![];
+        let mut co_requisites: Vec<usize> = vec![];
+        if let Some(requisites_node) = course[3].first_child() {
+            pre_requisites = get_pre_requisites(requisites_node);
+            co_requisites = get_co_requisites(requisites_node);
+            println!("pre: {:?}", pre_requisites);
+            println!("co:{:?}", co_requisites);
+        }
+
+        Course {
+            code,
+            name,
+            credits,
+            pre_requisites,
+            co_requisites,
+            semster: Semster {
+                order: 0,
+                name: "".to_string(),
+            },
+        }
+    }
+}
+
+// helper functions
+
+// " [0817-140] تفاضل وتكامل 1 - غير متزامن"
+fn get_pre_requisites(node: Node) -> Vec<usize> {
+    get_requisites(node, &|course: &String| course.contains("غير متزامن"))
+}
+// " [0817-140] تفاضل وتكامل 1 متزامن"
+fn get_co_requisites(node: Node) -> Vec<usize> {
+    get_requisites(node, &|course: &String| !course.contains("غير متزامن"))
+}
+
+fn get_requisites(node: Node, filter: &dyn Fn(&String) -> bool) -> Vec<usize> {
+    // the inner attribute contains html (bruh)
+    // like this: `<div data-content="<span/>"/>`
+    let data_content = node.attr("data-content").unwrap_or("");
+    let doc = Document::from(data_content);
+
+    doc.find(Name("div"))
+        .map(|node| node.text())
+        .filter(|course| filter(course))
+        .map(get_code)
+        .collect()
+}
+
+// input
+// " [0817-140] تفاضل وتكامل 1 - غير متزامن"
+// output
+// 0817140
+fn get_code(raw: String) -> usize {
+    let first = raw.find('[').unwrap() + 1;
+    let last = raw.find(']').unwrap() - 1;
+    let raw = raw.replace("-", "");
+
+    raw[first..last].parse::<usize>().unwrap()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip").await?.text().await?;
-    println!("{:#?}", resp);
-    Ok(())
+mod course;
+
+use course::Course;
+use select::document::Document;
+use select::predicate::Name;
+
+fn main() {
+    let document = Document::from(include_str!("../plan.html"));
+    let mut parsed_courses: Vec<Course> = vec![];
+
+    for (i, semster) in document.find(Name("tbody")).enumerate() {
+        for courses in semster.find(Name("tr")) {
+            let course = Course::from(courses);
+            parsed_courses.push(course.clone());
+
+            println!("{}", course.code);
+            // println!("{:#?}", course)
+        }
+    }
 }
